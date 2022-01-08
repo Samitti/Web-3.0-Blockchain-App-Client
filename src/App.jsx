c5c95b694eea9419ba1c56b5342940c59dbcbb13
Samitti <samuelteweldebrhan@gmail.com>
2022-01-08 10:40:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
